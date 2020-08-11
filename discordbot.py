d692ce38f4db6f25a1d58a503d3c2be576f42146
arurunosashimi <69537380+arurunosashimi@users.noreply.github.com>
2020-08-12 06:08:48 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -17,5 +17,22 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
-
+def simple_dice(dice_size, dice_num):
+    dice_val = np.array([], dtype=np.int64)
+    for i in range(dice_num):
+        dice_val = np.append(dice_val, dice(dice_size))
+    msg = 'dice: ' + str(np.sum(dice_val)) + ' = ' + str(dice_val)
+    return msg
+    
+    
+ 
+    
+    
+    
+    
+    
+    
+    
+    
+    
 bot.run(token)
